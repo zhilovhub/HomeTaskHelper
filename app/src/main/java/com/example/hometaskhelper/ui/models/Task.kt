fdf023c4147193893792b2dceb7be0530841b912
Ilya Zhilov <zhilovgithub@gmail.com>
2023-09-21 06:44:47 +0300

    data class Task

---
+++ app/src/main/java/com/example/hometaskhelper/ui/models/Task.kt
@@ -0,0 +1,9 @@
+package com.example.hometaskhelper.ui.models
+
+
+data class Task(
+    val name: String,
+    val date: String,
+    val description: String,
+    val finished: Boolean
+)
--- app/src/main/java/com/example/hometaskhelper/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/hometaskhelper/ui/screens/MainScreen.kt
@@ -3,6 +3,8 @@ package com.example.hometaskhelper.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -10,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hometaskhelper.ui.models.Task
 import com.example.hometaskhelper.ui.theme.HomeTaskHelperTheme
 
 
@@ -38,7 +40,34 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 fontFamily = FontFamily.SansSerif,
                 textAlign = TextAlign.Center
             )
+            Tasks()
         }
+    }
+}
+
+
+@Composable
+fun Tasks(tasks: List<Task> = listOf(
+    Task("Матан", "21.09.23", "1-4 номера без букв А", false),
+    Task("Линал", "22.09.23", "1-8 номера без букв Б", true),
+    Task("История", "23.09.23", "Эссе", false),
+)) {
+
+    LazyColumn {
+        items(tasks) {task ->
+            Task(task)
+        }
+    }
+
+}
+
+
+@Composable
+fun Task(task: Task) {
+    Column {
+        Text(text = task.name)
+        Text(text = task.date)
+        Text(text = task.description)
     }
 }
 

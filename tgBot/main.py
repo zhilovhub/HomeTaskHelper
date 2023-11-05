d2f0bc777b51b716981dae0335874042b4f285c5
mufln <forest31play@gmail.com>
2023-11-05 20:14:18 +0300

    webhook setup try 3

--- tgBot/main.py
+++ tgBot/main.py
@@ -18,13 +18,13 @@ async def dropWebhook(bot):
 
 async def makeWebhook(bot):
     await dropWebhook(bot)
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",certificate=FSInputFile(SSL_PEM))
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",certificate=FSInputFile(SSL_PEM),secret_token=WEBHOOK_SECRET)
 
 
 def setupWebApp(bot,dp):
     dp.startup.register(makeWebhook)
     app = web.Application()
-    webhookRequestHandler = SimpleRequestHandler(bot,dp)
+    webhookRequestHandler = SimpleRequestHandler(bot,dp,WEBHOOK_SECRET)
     webhookRequestHandler.register(app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
@@ -39,16 +39,17 @@ def setupBot():
     return bot,dp
 
 
-def main():
-    startBot(*setupBot())
-
-
-def startBot(bot,dp):
-    if platform.system() in ["Darwin","Windows"]:
-        dropWebhook(bot)
-        dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+async def main():
+    bot,dp = setupBot()
+    if platform.system() in ["Darwin", "Windows"]:
+        await startBot(bot,dp)
     else:
         setupWebApp(bot,dp)
+
+
+async def startBot(bot,dp):
+    await dropWebhook(bot)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 if __name__=="__main__":

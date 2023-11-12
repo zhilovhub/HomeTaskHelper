9bae9c7b80a8546db6f57368fb099e895ef13197
mufln <forest31play@gmail.com>
2023-11-12 13:14:12 +0300

    cfg rewrite

--- tgBot/cfg.py
+++ tgBot/cfg.py
@@ -1,14 +1,17 @@
 import os
-
-TOKEN = "1634628039:AAGbIRu7yNWoIPfSbT8cqc9bq9ebxNc0oRs"
+import platform
+if platform.system() in ("Darwin","Windows"):
+    TOKEN = os.environ.get("TEST_BOT_TOKEN")
+else:
+    TOKEN = os.environ.get("TT_BOT_TOKEN")
 WEB_SERVER_HOST = "127.0.0.1"
 WEB_SERVER_PORT = 8080
 WEBHOOK_PATH = "/webhook1/"
 SSL_CERT = os.environ.get("SSL_CERT_PATH")
 SSL_KEY = os.environ.get("SSL_KEY_PATH")
-WEBHOOK_SECRET = "Aoaoaommmm"
-WEBHOOK_URL = "https://194.87.252.94:443"
-DB_PATH = "TeleTask"
-DB_USER = "tg_bot"
-DB_PASSWORD = "Aa@12345678"
+WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET")
+WEBHOOK_URL = os.environ.get("WEBHOOK_BASE_URL")
+DB_PATH = os.environ.get("TT_BOT_DB")
+DB_USER = os.environ.get("TT_BOT_DB_USER")
+DB_PASSWORD = os.environ.get("TT_BOT_DB_PASSWORD")
 DB_HOST = "localhost"
